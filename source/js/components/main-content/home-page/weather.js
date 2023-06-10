5a3c8e0bd52d543de7643357f1ada2438c6e61a5
MuammarRizal31 <muammarrizal2002@gmail.com>
2023-06-10 12:39:55 +0700

    finish API

--- source/js/components/main-content/home-page/weather.js
+++ source/js/components/main-content/home-page/weather.js
@@ -26,60 +26,7 @@ class Weather extends HTMLElement {
       </div>
     </div>
     <div class="row">
-      <div class="sidebar col-md-3">
-        <div class="card-body">
-          <div class="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary" style="width: 280px">
-            <h3>Filter Search</h3>
-            <hr />
-            <ul class="nav nav-pills flex-column mb-auto">
-              <h4>Situasi</h4>
-              <li>
-                <label class="cyberpunk-checkbox-label">
-                  <input class="cyberpunk-checkbox" type="checkbox" />
-                  Cerah</label
-                >
-              </li>
-              <li>
-                <label class="cyberpunk-checkbox-label">
-                  <input class="cyberpunk-checkbox" type="checkbox" />
-                  Berawan</label
-                >
-              </li>
-              <li>
-                <label class="cyberpunk-checkbox-label">
-                  <input class="cyberpunk-checkbox" type="checkbox" />
-                  Hujan</label
-                >
-              </li>
-              <li>
-                <label class="cyberpunk-checkbox-label">
-                  <input class="cyberpunk-checkbox" type="checkbox" />
-                  Badai</label
-                >
-              </li>
-            </ul>
-            <hr />
-            <ul class="nav nav-pills flex-column mb-auto">
-              <h4>Satuan</h4>
-              <li>
-                <label class="cyberpunk-checkbox-label">
-                  <input class="cyberpunk-checkbox" type="checkbox" />
-                  °C</label
-                >
-              </li>
-              <li>
-                <label class="cyberpunk-checkbox-label">
-                  <input class="cyberpunk-checkbox" type="checkbox" />
-                  °F</label
-                >
-              </li>
-            </ul>
-            <hr />
-            <button class="button_submit"><span>Button</span><i></i></button>
-          </div>
-        </div>
-      </div>
-      <div class="card_weather col-md-9 col-md-12-responsive">
+      <div class="card_weather col-md-12-responsive">
         <div class="row weather-cards"></div>
       </div>
     </div>
